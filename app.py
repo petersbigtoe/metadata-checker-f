eae2e4a1c38d8e4e12c4f16d6d3935bf8f8cb7d7
nichole <23000184@myrp.edu.sg>
2025-07-19 23:54:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file, render_template_string
+from flask import Flask, request, send_file, render_template, render_template_string
 import subprocess
 import os
 from werkzeug.utils import secure_filename
